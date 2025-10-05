1e9a1a4c261a14e7f5e13ca7f175c523f8454a52
Levgj <127439153+levgj@users.noreply.github.com>
2025-10-05 11:17:35 +0100

    Creación del temporizador
    Creado el temporizador de los diez segundos, aunque aún da un error: "Cannot implicitly convert type float to bool".
    
    Notas
    Preguntar a Elíseo cómo llamar la variable de una script en otra. No basta con hacerla public.

--- Assets/Scripts/SpaceCount.cs
+++ Assets/Scripts/SpaceCount.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class SpaceCount : MonoBehaviour
 
 {
-    int spacePressedCount = 0;
+    public int spacePressedCount = 0;
+    float Timer = 0.0f; 
+    bool isPlaying = false; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,11 +16,29 @@ public class SpaceCount : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //este script indica las acciones al presionar el espacio
         if(Input.GetKeyDown("space"))
         {
             spacePressedCount ++ ;
             Debug.Log("space has been pressed" + spacePressedCount + "times");
             
         }
+
+        if(spacePressedCount > 0)
+        {
+            isPlaying = true;
+        }
+
+        if(isPlaying == true)
+        {
+            Timer = Timer++;
+            Debug.Log("Timer");
+        }
+
+        if(Timer = 10.0f)
+        {
+            isPlaying = false;
+        }
+
     }
 }
